d58f98f3c2d54cfde90205d18f22251575dfa29c
Mayte Giménez <la.reina.roja@gmail.com>
2012-04-23 16:25:07 +0200

    Cloning from local server

--- Server/clients/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse 
-import datetime
-
-def index(request):
-    now = datetime.datetime.now()
-    html = "<html><body>Ahora es %s </body></html>" % now
-    #t = loader.get_template('clients/index.html')
-    #c = Context()
-    return HttpResponse(html)
--- Server/tracks/views.py
+++ Server/tracks/views.py
@@ -4,6 +4,6 @@ from tracks.models import Track
 
 def index(request):
     tracks = Track.objects.all()
-    t = loader.get_template('tracks/index.html')
+    t = loader.get_template('index.html')
     c = Context({'tracks': tracks})
     return HttpResponse(t.render(c))
