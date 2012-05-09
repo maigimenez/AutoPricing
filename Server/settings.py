2b9795d8011c52fee573fc442a3ec320154fb2ac
Mayte Giménez <la.reina.roja@gmail.com>
2012-05-09 13:36:41 +0200

    Working with api urls

--- Server/settings.py
+++ Server/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.gis',
     #User applications
     'testing',
+    'api',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
